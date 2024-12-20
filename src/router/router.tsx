b2ff13f9fe4e4733f7fde3c146afd638d79f7a60
ayodtrader12 <ibrahimpopoola292@gmail.com>
2024-12-20 13:47:05 -0800

    file structure

---
+++ src/router/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import AuthLayout from "../layout/AuthLayout";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Home from "../pages/home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      // Add more routes for other pages
+    ],
+  },
+]);
