9b610c54968f610d159deaf5dd861ff1cf53327a
esckles <davidsamzzywilliams1234@gmail.com>
2024-12-20 18:42:00 +0100

    hh

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
       <div className="w-[100%] h-full bg-[#00897B] flex justify-center items-center">
         <div className="h-[70vh] w-[60vw] bg-[#E8F5E9] flex">
           <div className="w-[50%] h-[100%]"></div>
-          <div className="w-[50%] h-[100%]">
+          <div className="w-[55%] h-[100%]">
             <img className="w-[100%] h-[100%] opacity-85" src={pix} alt="" />
           </div>
         </div>
