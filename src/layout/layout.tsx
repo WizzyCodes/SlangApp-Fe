1321ffedbde86fc336127f715b17f025c046ebc4
esckles <davidsamzzywilliams1234@gmail.com>
2024-12-20 17:21:40 +0100

    ui

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default layout;
