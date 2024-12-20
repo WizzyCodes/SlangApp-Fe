8d7cb584407af9447e07feb83bd2e06f366ef5bc
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-12-20 17:32:09 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { Provider } from "react-redux";
 
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import { store } from "./global/store";
+
+let persistor = persistStore(store);
 const App = () => {
   return (
     <div>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <RouterProvider router={router} />{" "}
+        </PersistGate>
+      </Provider>
     </div>
   );
 };
