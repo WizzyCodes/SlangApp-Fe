f5eca14f7b917e9046aecc269612de26b269206a
ayodtrader12 <ibrahimpopoola292@gmail.com>
2024-12-20 17:41:41 -0800

    little

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -2,6 +2,23 @@ import axios from "axios";
 
 const URL: string = "https://localhost:3440/api/user";
 
+export const createUser = (data: any) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    return axios
+      .post(`${URL}/api/user/create-user`, data, config)
+
+      .then((res) => {
+        return res?.data;
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const LoginUser = async (data: any) => {
   try {
     return axios.post(`${URL}/login`, data).then((res) => {
